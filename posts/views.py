927de34fea6b7ea628ded9548306bd4c6beac4fb
Micah Gordon <micahgordon66@gmail.com>
2018-06-28 12:21:10 +0300

    create basic file structure for rendering posts.html

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def posts(request):
+    context = {
+        'message': 'hello'
+    }
+    return render(request, 'posts.html', context)
