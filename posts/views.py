e78583a9b83fc6740ab255afdeedf8c29ef2f746
Micah Gordon <micahgordon66@gmail.com>
2018-07-01 10:20:45 +0300

    Redirect user to /posts/ when submitting a new post

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_protect
 from django.utils import timezone
 from .models import Post
@@ -9,13 +9,14 @@ from .forms import PostForm
 @csrf_protect
 def posts(request):
     posts = Post.objects.all()
+    date = timezone.now()
     if request.method == 'POST':
         post = PostForm(request.POST)
         if post.is_valid():
             Post.objects.create(**post.cleaned_data)
+        return redirect('/posts/')
     else:
         post = PostForm()
-    date = timezone.now()
     context = {
         'post': post,
         'posts': posts,
